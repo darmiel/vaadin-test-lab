0da925ce2a947358ad79d3f6de7d94cc4df216ee
Daaniel <hi@daaniel.de>
2019-12-07 12:57:25 +0100

    Test ComboBox

--- src/main/java/it/statzner/testlab/spring/MainView.java
+++ src/main/java/it/statzner/testlab/spring/MainView.java
@@ -1,21 +1,22 @@
 package it.statzner.testlab.spring;
 
-import com.vaadin.flow.component.button.Button;
+import com.vaadin.annotations.Viewport;
 import com.vaadin.flow.component.html.H1;
-import com.vaadin.flow.component.notification.Notification;
-import com.vaadin.flow.theme.Theme;
-import com.vaadin.flow.theme.lumo.Lumo;
-import org.springframework.beans.factory.annotation.Autowired;
-
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.server.PWA;
 
-@Route
+@Route()
+@Viewport("width=device-width, minimum-scale=1, initial-scale=1, user-scalable=yes, viewport-fit=cover")
+@PWA(
+        name = "Daniel's TestLab",
+        shortName = "DTL",
+        description = "Some experiments"
+)
 public class MainView extends VerticalLayout {
 
     public MainView() {
-        add(new H1("Daniel's Test Lab"));
+        add(new H1("Daniels Test-Lab"));
     }
 
 }
--- src/main/java/it/statzner/testlab/spring/modules/lanis/LanisView.java
+++ src/main/java/it/statzner/testlab/spring/modules/lanis/LanisView.java
@@ -1,22 +1,24 @@
-package it.statzner.testlab.spring.modules;
+package it.statzner.testlab.spring.modules.lanis;
 
-import com.sun.org.apache.bcel.internal.generic.LSTORE;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.checkbox.Checkbox;
+import com.vaadin.flow.component.combobox.ComboBox;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.html.*;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.progressbar.ProgressBar;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.data.value.ValueChangeMode;
 import com.vaadin.flow.router.Route;
+import com.vaadin.flow.theme.Theme;
+import com.vaadin.flow.theme.material.Material;
 import it.statzner.testlab.spring.modules.lanis.model.Role;
 import it.statzner.testlab.spring.modules.lanis.model.User;
 import it.statzner.testlab.spring.modules.lanis.reader.UserListFileReader;
 import it.statzner.testlab.spring.modules.lanis.thr.InvalidUserException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.task.TaskExecutionAutoConfiguration;
 
 import java.io.File;
 import java.io.IOException;
@@ -24,9 +26,9 @@ import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 @Route("lanis")
+@Theme(value = Material.class, variant = Material.DARK)
 public class LanisView extends VerticalLayout {
 
     private UserListFileReader lanisReader;
@@ -35,15 +37,16 @@ public class LanisView extends VerticalLayout {
     private String userNameFilter = "";
     private boolean onlyTeacherFilter = false;
 
-
     private Grid<User> grid;
     private Paragraph foundUsersParagraph;
+    private ProgressBar progressBar;
 
     public LanisView(@Autowired UserListFileReader lanisReader) {
         setDefaultHorizontalComponentAlignment(Alignment.CENTER);
         getThemeList().add("dark");
 
         this.lanisReader = lanisReader;
+        this.userList = new ArrayList<>();
 
         // Grid
         this.grid = new Grid<>();
@@ -53,29 +56,33 @@ public class LanisView extends VerticalLayout {
         this.grid.addColumn(User::getClassName).setHeader("Klasse");
         this.grid.addColumn(User::getPassword).setHeader("Passwort");
 
-        this.userList = new ArrayList<>();
+        // ProgressBar
+        this.progressBar = new ProgressBar();
 
-
-        add(
-                new H1("LANiS Decoder"),
-                new Paragraph("Decrypts LANiS Users")
-        );
-
+        // Text Path Field
+        /*
         TextField pathField = new TextField();
         pathField.setValue("userliste_kss.txt");
         pathField.setClearButtonVisible(true);
+        pathField.setHeightFull();
+        pathField.setWidth("70%");
+         */
 
+        ComboBox<String> comboBox = new ComboBox<>("Browsers");
+        comboBox.setItems("Google Chrome", "Mozilla Firefox", "Opera",
+                "Apple Safari", "Microsoft Edge");
+
+        comboBox.addValueChangeListener(event -> {
+        });
+
+        // Decode Button
         Button submitButton = new Button("Decode");
         submitButton.setThemeName("primary");
         submitButton.addClickListener(click -> {
-            loadData(pathField.getValue());
+            loadData(comboBox.getValue());
         });
 
-        add(new HorizontalLayout(
-                pathField,
-                submitButton
-        ));
-
+        // Textfield
         TextField searchField = new TextField();
         searchField.setPlaceholder("Filter ...");
         searchField.setWidth("100%");
@@ -86,6 +93,7 @@ public class LanisView extends VerticalLayout {
             updateList();
         });
 
+        // Found Info
         this.foundUsersParagraph = new Paragraph("Gefunden: 0");
         this.foundUsersParagraph.setWidth("100%");
 
@@ -97,25 +105,69 @@ public class LanisView extends VerticalLayout {
             this.updateList();
         });
 
-        add(
+
+        // Select Teil
+        HorizontalLayout selectLayout = new HorizontalLayout(
+                comboBox,
+                submitButton
+        );
+        selectLayout.setWidth("30%");
+        selectLayout.setAlignItems(Alignment.CENTER);
+
+        Div div = new Div();
+        div.setHeightFull();
+        div.setWidth("70%");
+        div.add(
+
+                // Select File
+                new Hr(),
+                selectLayout,
+
+                // Search
                 new Hr(),
                 new HorizontalLayout(
                         searchField,
                         this.foundUsersParagraph,
                         onlyTeacherCheck
                 ),
+                this.progressBar,
                 this.grid
+
         );
 
+        add(
+                // Header
+                new Header(
+                        new H1("LANiS-Decrypt"),
+                        new Paragraph("Decrypts LANiS Users")
+                ),
+
+                // Main Div
+                div
+        );
     }
 
     public void loadData(String path) {
+        // Progress start
+        this.progressBar.setVisible(true);
+        long startTime = System.currentTimeMillis();
+
         try {
             this.userList = lanisReader.readUserList(new File(path));
+
+            // Progress end
+            this.progressBar.setMax(this.userList.size());
+
             this.updateList();
         } catch (IOException | ParseException | InvalidUserException e) {
             Notification.show("Exception: " + e.getMessage());
         }
+
+        Notification.show(
+                "Data loaded in " + (System.currentTimeMillis() - startTime) + " ms!",
+                1000,
+                Notification.Position.MIDDLE
+        );
     }
 
     public void updateList() {
@@ -134,7 +186,9 @@ public class LanisView extends VerticalLayout {
 
         this.grid.setItems(items);
 
-        this.foundUsersParagraph.setText("Gefunden: " + items.size());
+        this.foundUsersParagraph.setText("Gefunden: " + items.size() + " (" +
+                Math.round((double)items.size() / (double)this.userList.size()*100D) + "%)");
+        this.progressBar.setValue(items.size());
     }
 
 }
